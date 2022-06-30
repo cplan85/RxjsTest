d1037583fd59dc31083f3f7da10fbb24885fff06
Carlos Planchart <carlos_planchart@live.com>
2022-06-30 13:13:01 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RxjsTest';
+  obs: any;
+
+  ngOnInit() {
+    this.obs = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+  }
+
+  rxjsFunction() {
+    console.log('just before subscribe');
+this.obs.subscribe({
+  next(x: number) { console.log('got value ' + x); },
+  error(err: {}) { console.error('something wrong occurred: ' + err); },
+  complete() { console.log('done'); }
+});
+console.log('just after subscribe');  
+  }
+
 }
